e539cbeaa6eae5896f2ff96b10d2229204cbbdb2
peytont243 <peytont243@gmail.com>
2020-06-10 22:57:26 -0500

    implemented some of the BankingManager prompts for authentication, implemented some of Account's basic functionality, and created a basic text file for storing accounts

--- Account.java
+++ Account.java
@@ -2,4 +2,90 @@ package BankingManager;
 
 public class Account {
     
+    private String username;    // account username
+    private String password;    // account password
+    private int balance;        // account balance
+
+    /**
+     * Default constructor
+     * Creates an empty account with a default username and password and initializes it with a zero balance
+     */
+    public Account () {
+        this.username = "noname";
+        this.password = "password";
+        this.balance = 0;
+    }
+
+    /**
+     * Creates an account with zero balance with the provided username and password
+     * @param username account username
+     * @param password account password
+     */
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.balance = 0;
+    }
+
+    /**
+     * Constructs an Account with a provided username, password, and balance
+     * @param username account username
+     * @param password account password
+     * @param balance account balance
+     */
+    public Account(String username, String password, int balance) {
+        this.username = username;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    /**
+     * Gets the username for this Account
+     * @return this Account's username
+     */
+    public String getUsername() {
+        return this.username;
+    }
+
+    /**
+     * Gets the current balance for this Account
+     * @return this Account's balance
+     */
+    public int getBalance() {
+        return this.balance;
+    }
+
+    /**
+     * Checks if the provided password matches this Account's password
+     * @param password the password to compare with the Account's password
+     * @return true if the passwords match, false otherwise
+     */
+    public boolean validatePassword(String password) {
+        if (this.password.equals(password)) return true;
+        else return false;
+    }
+
+    /**
+     * Update this Account's username
+     * @param newUsername the new username for this Account
+     */
+    public void setUsername(String newUsername) {
+        this.username = newUsername;
+    }
+
+    /**
+     * Update this Account's password
+     * @param oldPassword the current password on the Account
+     * @param newPassword the new password to set
+     * @return true if the password change is successful, false otherwise
+     */
+    public boolean setPassword(String oldPassword, String newPassword) {
+        boolean changed = false;
+        if (this.password.equals(oldPassword)){
+            this.password = newPassword;
+            changed = true;
+        }
+        return changed;
+    }
+
 }
--- BankingManager.java
+++ BankingManager.java
@@ -1,5 +1,58 @@
 package BankingManager;
 
+import javax.swing.JOptionPane;
+import java.io.File;
+import java.util.Scanner;
+
 public class BankingManager {
     
+    /**
+     * Compares input username and password to confirm they are a valid combination
+     * @param usernameInput the user's inputted username to test
+     * @param passwordInput the user's inputted password to test
+     * @return true if authentication is successful, false otherwise
+     */
+    private static boolean authenticate(String usernameInput, String passwordInput) {
+        boolean authentication = false;
+
+        return authentication;
+    }
+
+    public static void main(String args[]) {
+
+        // declare variables
+        String title = "PT Banking";
+        
+        // welcome message
+        JOptionPane.showMessageDialog(null,"Welcome to PT Banking! Where none of your actual banking needs are met, and it's all just a simulation.", title, JOptionPane.OK_OPTION);
+
+        // start login authentication process
+        String usernameInput = "";
+        String passwordInput = "";
+        int attempts = 0;
+        boolean authenticated = false;
+
+        while (attempts < 3) {
+
+            usernameInput = JOptionPane.showInputDialog(null,"Please enter your username.", title, JOptionPane.OK_OPTION);
+            passwordInput = JOptionPane.showInputDialog(null,"Please enter your password.", title, JOptionPane.OK_OPTION);
+
+            authenticated = authenticate(usernameInput, passwordInput);
+            if (authenticated) break;
+            else {
+                JOptionPane.showMessageDialog(null, "Incorrect username and/or password.", title, JOptionPane.ERROR_MESSAGE);
+            }
+
+            attempts++;
+
+            if (attempts == 3) {
+                JOptionPane.showMessageDialog(null, "You have run out of login attempts. Goodbye.", title, JOptionPane.ERROR_MESSAGE);
+            }
+
+        }
+
+        // if login successful, start main application process
+
+    }
+
 }
